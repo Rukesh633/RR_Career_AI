bb1ce35adbb3b30611aad791aef77ff82a26e13a
Rukesh633 <rukeshrukku123@gmail.com>
2026-04-04 16:14:47 +0530

    Added React-ga4

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-ZWV8VFSQ89"); // Your ID here
+ReactGA.send("pageview");
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
